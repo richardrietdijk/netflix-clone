3f63505a6638a979fc34fc1e5ddf830e68b7a464
Richard Rietdijk <richardrietdijk@gmail.com>
2020-09-03 16:46:57 +0200

    implement hero

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
-
+import Banner from './Banner';
 //
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      {/* Nav */}
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
